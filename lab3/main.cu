47f98dfcad5ff75cc35e46850acb2d158bdbc67d
lcaonmst <kzwierzchowski34@gmail.com>
2021-03-19 17:29:38 +0100

    First program

--- lab3/main.cu
+++ lab3/main.cu
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <stdio.h>
+
+__global__ kernel() {
+    while (true) {};
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    const int N = 10;
+
+//    int* gpu_arr;
+//    cudaMalloc(&gpu_arr, N * sizeof(int));
+    kernel<<<1, 256>>>();
+    cout << "siema";
 }
